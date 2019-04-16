707b019de384f33c0ea54736faddea1cf1bfd878
eminarium <merdan.durdiyev@gmail.com>
2019-04-16 18:53:51 +0500

    Designed view file for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,7 @@ class ContactsController < ApplicationController
   # GET /contacts.json
   def index
     @contacts = Contact.all
+    @categories = Category.all.order('id ASC')
   end
 
   # GET /contacts/1
